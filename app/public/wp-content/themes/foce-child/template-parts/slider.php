cfd29ad38362413c109d799c394cfa28f64a2209
wefr1 <weshdzdz1@gmail.com>
2024-02-23 20:44:55 +0100

    mise à jour

--- app/public/wp-content/themes/foce-child/template-parts/slider.php
+++ app/public/wp-content/themes/foce-child/template-parts/slider.php
@@ -1,25 +1,24 @@
 <?php
-    $args = array(
-        'post_type' => 'characters',
-        'posts_per_page' => -1,
-        'meta_key'  => '_main_char_field',
-        'orderby'   => 'meta_value_num',
-    );
-    $characters_query = new WP_Query($args);
+$args = array(
+    'post_type' => 'characters',
+    'posts_per_page' => -1,
+    'meta_key'  => '_main_char_field',
+    'orderby'   => 'meta_value_num',
+);
+$characters_query = new WP_Query($args);
 ?>
 
-<swiper-container class="mySwiper" loop="true" effect="coverflow" slides-per-view="3">
-
-<?php
-
-      
-while ( $characters_query->have_posts() ) {
-  $characters_query->the_post();          
-  echo '<swiper-slide>' .get_the_post_thumbnail( get_the_ID(), 'full' ), the_title(). '</swiper-slide>  ';  
-  
-}
-?>
-
-</swiper-container>
-
-
+<div class="swiper-container">
+    <div class="swiper-wrapper">
+        <?php while ($characters_query->have_posts()) : $characters_query->the_post(); ?>
+            <div class="swiper-slide">
+                <figure>
+                    <?php echo get_the_post_thumbnail(get_the_ID(), 'full'); ?>
+                    <figcaption>
+                        <h4><?php the_title(); ?></h4>
+                    </figcaption>
+                </figure>
+            </div>
+        <?php endwhile; ?>
+    </div>
+</div>
